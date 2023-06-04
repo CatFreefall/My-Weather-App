1ddb42fcebf72824173e47dfbbb2c506bc21edd7
kevin <kevinfng12@gmail.com>
2023-06-04 13:40:42 -0400

    styled city display box and added background

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useState } from "react";
 
+import "/src/styles/App.css";
+
 import { Header } from "./components/Header";
 import { CityInfo } from "./components/CityInfo";
 import { Forecast } from "./components/Forecast";
 
 export default function App() {
   return (
-    <div>
+    <div id="page-layout">
       <Header />
       <CityInfo />
       <Forecast />
--- src/components/CityInfo.jsx
+++ src/components/CityInfo.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
+import "/src/styles/CityInfo.css";
+
 export function CityInfo() {
   return (
-    <div>
-      <img src="/assets/icons/location-icon.png" alt="Map Icon"></img>
-      <h1 id="city-name">Toronto | CA</h1>
+    <div id="city-info">
+      <div id="city-name">
+        <img src="/assets/icons/location-icon.png" alt="Map Icon"></img>
+        <h1>Toronto | CA</h1>
+      </div>
       <div id="condition-icon"></div>
       <h6 id="condition">Condition:</h6>
       <h6 id="wind-speed">Wind Speed:</h6>
-      <h6 id="wind angle">Wind Angle:</h6>
       <h6 id="temperature">Temperature:</h6>
+      <h6 id="wind-angle">Wind Angle:</h6>
     </div>
   );
 }
