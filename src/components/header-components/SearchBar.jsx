011c8793bbe4811ee33ff8eb429c8952606140d7
kevin <kevinfng12@gmail.com>
2023-06-09 22:44:54 -0400

    suggestions fetched from API. Still needs to be displayed

--- src/components/header-components/SearchBar.jsx
+++ src/components/header-components/SearchBar.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "/src/styles/header-component-styles/SearchBar.css";
 
 export function SearchBar({ onSearch }) {
   const [search, setSearch] = React.useState("");
+  const [suggestions, setSuggestions] = React.useState([]);
+
+  const fetchSuggestions = async (currentInput) => {
+    const options = {
+      method: "GET",
+    };
+
+    const host = "http://api.weatherapi.com/v1/search.json?";
+    const key = "c9ea430da233494d80d231236222412";
+    const input = currentInput;
+    const callURL = host + "key=" + key + "&q=" + input;
+
+    try {
+      const response = await fetch(callURL, options);
+      const fetchedSuggestions = await response.json();
+      setSuggestions(fetchedSuggestions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // This hook is needed to get the updated suggestions array (instead of previous).
+  useEffect(() => {
+    console.log("Updated suggestions:", suggestions);
+  }, [suggestions]);
 
   const getCurrentInput = (e) => {
-    setSearch(e.target.value);
-    console.log("CURRENT TEXT INPUTTED", e.target.value);
+    const currUserInput = e.target.value;
+    setSearch(currUserInput);
+
+    //API does not fetch anything if input.length < 3. This prevents unnecessary API calls.
+    currUserInput.length >= 3 ? fetchSuggestions(currUserInput) : null;
   };
 
   const submitInput = (e) => {
@@ -35,6 +63,14 @@ export function SearchBar({ onSearch }) {
           ></img>
         </button>
       </form>
+
+      <div id="suggestions">
+        {suggestions.length > 0
+          ? suggestions.map((suggestion) => (
+              <div key={suggestion.id} className="single-suggestion"></div>
+            ))
+          : null}
+      </div>
     </div>
   );
 }
